7c893c47722ddc576c4748000c0bb98d8b694b85
Xibitol <xibitol@pimous.dev>
2025-05-10 15:50:04 +0200

    Configured a dockerized Caddy fallback server.

---
+++ fallback/src/config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export DOCKER_FILE=$SCRIPT_DIR/Dockerfile
+
+export DOCKER_IMAGE_REFERENCE=pimousservers/fallback:2.10.0-alpine
+export DOCKER_CONTAINER_NAME=ps-fallback
+
+export DOCKER_CONFIG_VOLUME_NAME=$DOCKER_CONTAINER_NAME-config
+export DOCKER_DATA_VOLUME_NAME=$DOCKER_CONTAINER_NAME-data
+export EXPOSED_PORT=31001
+export CONTAINER_IP=172.17.2.2
