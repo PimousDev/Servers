68f6fb4dea65a1df3283f5c4cff892e05b5a9c55
Xibitol <xibitol@pimous.dev>
2026-06-14 20:06:27 +0200

    Added a maven repository powered by Reposilite.

---
+++ maven/deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Pimous Servers (Scripts and Docker files)
+# Copyright &copy; 2025 - Pimous Dev. (https://www.pimous.dev/)
+#
+# This script is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# The latter are distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# No copy of the license is bundled with the script (As it is posted in a GitHub
+# gist). Please see https://www.gnu.org/licenses/.
+
+SCRIPT_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+
+# ---
+mode=${1-"prod"}
+
+# ---
+cd "$SCRIPT_DIR" || exit
+
+if [[ $mode = "prod" ]]; then
+	docker compose up --build -d
+else
+	docker compose up --build
+fi
+
+cd - >/dev/null || exit
