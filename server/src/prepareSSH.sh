62e5501379b84eed9a8e97a96896988f70c0c64a
Xibitol <xibitol@pimous.dev>
2026-07-07 21:09:19 +0200

    Added configuration of openssh.

---
+++ server/src/prepareSSH.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Pimous Servers (Scripts and Docker files)
+# Copyright &copy; 2026 - Pimous Dev. (https://www.pimous.dev/)
+#
+# This script is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# The latter are distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# No copy of the license is bundled with the script. Please see
+# https://www.gnu.org/licenses/.
+#-------------------------------------------------------------------------------
+# @throw 1 Unkown error.
+# @throw 2 Bad usage.
+#-------------------------------------------------------------------------------
+
+groupadd ssh-allowed -U debian
+groupadd sftp-allowed
+
+mkdir /home/sftphomes
+chmod 710 /home/sftphomes
+chown root:sftp-allowed /home/sftphomes
